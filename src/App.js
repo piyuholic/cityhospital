264d7ea5f228b677a0f9f44439d8035c12a249a2
piyuholic <piyushkatariya.ppsv@gmail.com>
2022-08-25 11:07:05 +0530

    25-08-integration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header/Header';
+import Footer from './Component/Footer/Footer';
+import Home from './Container/Home/Home';
+import Department from './Container/Deprtments/Departments';
+import Doctors from './Container/Doctors/Doctors';
+import About from './Container/About/About';
+import Contact from './Container/Contact/Contact';
+import Appointment from './Container/Appointment/Appointment';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Switch>
+      <Route exact path={"/"} component={Home} />
+      <Route exact path={"/department"} component={Department} />
+    {/* <Home /> */}
+    {/* <Department /> */}
+    {/* <Doctors /> */}
+    {/* <About /> */}
+    {/* <Contact /> */}
+    <Appointment/>
+    </Switch>
+    <Footer />
+    </>
   );
 }
 
